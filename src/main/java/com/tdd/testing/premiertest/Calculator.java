01142c5b1f5e0a1f084ee7577a3ee853a0e18179
shahin <shahinbavili@gmail.com>
2025-11-22 13:08:45 +0100

    Create Calculate test class

---
+++ src/main/java/com/tdd/testing/premiertest/Calculator.java
@@ -0,0 +1,7 @@
+package com.tdd.testing.premiertest;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/tdd/testing/premiertest/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.tdd.testing.premiertest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void testAddTwoPositiveNumbers() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+        Calculator calculator = new Calculator();
+
+        // Act
+        int somme = calculator.add(a, b);
+
+        // Assert
+        assertEquals(5, somme);
+    }
+
+}
