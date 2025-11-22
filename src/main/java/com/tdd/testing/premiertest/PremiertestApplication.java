fb119d4384f43e0139750f58fc7c88254fb6e867
shahin <shahinbavili@gmail.com>
2025-11-22 12:55:10 +0100

    initial commit

---
+++ src/main/java/com/tdd/testing/premiertest/PremiertestApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.testing.premiertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/testing/premiertest/PremiertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.testing.premiertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
