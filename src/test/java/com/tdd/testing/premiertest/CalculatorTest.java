6a56d18684428187fa815448b26e4b4478bf753d
shahin <shahinbavili@gmail.com>
2025-12-08 12:03:01 +0100

    Structure tests with Annotations

--- src/test/java/com/tdd/testing/premiertest/CalculatorTest.java
+++ src/test/java/com/tdd/testing/premiertest/CalculatorTest.java
@@ -1,20 +1,91 @@
 package com.tdd.testing.premiertest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
+    private static Instant startedAt;
+    private Calculator calculatorUnderTest;
+
+    @BeforeEach
+    public void initCalculator() {
+        System.out.println("Appel avant chaque test");
+        calculatorUnderTest = new Calculator();
+    }
+
+    @AfterEach
+    public void undefineCalculator() {
+        System.out.println("Appel après chaque test");
+        calculatorUnderTest = null;
+    }
+
+    @BeforeAll
+    public static void initStartingTime() {
+        System.out.println("Appel avant tous les test");
+        startedAt = Instant.now();
+    }
+
+    @AfterAll
+    public static void showTestDuration() {
+        System.out.println("Appel après tous les test");
+        Instant endedAt = Instant.now();
+        long duration = Duration.between(startedAt, endedAt).toMillis();
+        System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+    }
+
+    @ParameterizedTest(name = "{0} x 0 doit être égal à 0")
+    @ValueSource(ints = {1, 2, 42, 1011, 5089})
+    public void multiply_shouldReturnZero_ofZeroWithMultipleIntegers(int arg) {
+//        Arrange — tout est prêt !
+
+//        Act -- Multiplier par zéro
+        int actualResult = calculatorUnderTest.multiply(0, arg);
+
+//        Assert — ça vaut toujours zéro
+        assertEquals(0, actualResult);
+    }
+
+    @ParameterizedTest(name = "{0} + {1} should equal to {2}")
+    @CsvSource({"1,1,2", "2,3,5", "42,57,99"})
+    public void add_should_return_the_sum_of_multiple_Integers(int arg1, int arg2, int expectedResult) {
+//        Arrange -- All is ready
+
+//        Act
+        int actualResult = calculatorUnderTest.add(arg1, arg2);
+
+//        Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Timeout(1)
+    @Test
+    public void longCalcule_should_compute_in_less_than_1_second() {
+//        Arrange
+
+//        Act
+        calculatorUnderTest.longCalculation();
+
+//    Assert
+//    ...
+
+    }
 
     @Test
     void testAddTwoPositiveNumbers() {
         // Arrange
         int a = 2;
         int b = 3;
-        Calculator calculator = new Calculator();
 
         // Act
-        int somme = calculator.add(a, b);
+        int somme = calculatorUnderTest.add(a, b);
 
         // Assert
         assertEquals(5, somme);
@@ -25,13 +96,11 @@ class CalculatorTest {
         //Arrange
         int a = 3;
         int b = 5;
-        Calculator calculator = new Calculator();
 
         //Act
-        int somme = calculator.multiply(a, b);
+        int product = calculatorUnderTest.multiply(a, b);
 
         //Assert
-        assertEquals(15, somme);
+        assertEquals(15, product);
     }
-
 }
