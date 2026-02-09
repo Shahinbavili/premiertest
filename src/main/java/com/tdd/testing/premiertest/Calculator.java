92f35d63e0bc2e1bb21c8c2ae0e3ee828ff2d612
shahin <shahinbavili@gmail.com>
2026-02-09 07:08:43 +0100

    Ignore IntelliJ coverage HTML reports

--- src/main/java/com/tdd/testing/premiertest/Calculator.java
+++ src/main/java/com/tdd/testing/premiertest/Calculator.java
@@ -8,7 +8,15 @@ public class Calculator {
         return a + b;
     }
 
+    public double add(double a, double b) {
+        return a + b;
+    }
+
     public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
         return a * b;
     }
 
